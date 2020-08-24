b295cb76a89015f69604bf45d75f1d5277cc5fa5
Koshal Singh <koshal.nirwan24@gmail.com>
2020-08-24 12:11:25 +0530

    functions removed

--- app.py
+++ app.py
@@ -9,11 +9,44 @@ def get_data():
     
     resp = requests.get('https://api.covid19india.org/v4/timeseries.json')
     df = pd.DataFrame(resp.json())
-    #df.columns
     return df
 
 df = get_data()
 
+def get_total_data(li,d):
+    """ Fetches and organises the data for covid cases in each state of India """
+    
+    dt=[]
+    des=[]
+    
+    for col in li:
+        if col=='KL':
+            for dat in df[col]['dates'].items():
+                dt.append(dat[0])
+                try:
+                    des.append(dat[1]['total'][d])
+                except:
+                    des.append('NaN')
+            df2 = pd.concat([pd.DataFrame(dt,columns=['Date']), pd.DataFrame(des,columns=['KL'])],axis=1)
+        
+        dt2=[]
+        des2=[]
+        if col != 'KL':   
+            df3 = pd.DataFrame()
+            for dat in df[col]['dates'].items():
+                dt2.append(dat[0])
+                try:
+                    des2.append(dat[1]['total'][d])
+                except:
+                    des2.append('NaN')
+            df3 = pd.concat([pd.DataFrame(dt2,columns=['Date']), pd.DataFrame(des2,columns=[col])],axis=1)
+            
+            df2 = pd.merge_ordered(df2,df3,on='Date')
+            
+    return df2
+       
+###### Data Extraction and Cleaning ###########
+    
 dicti = {'KL':'Kerala','AN':'Andaman & Nicobar','AP':'Andra Pradesh','AR':'Arunachal Pradesh','AS':'Assam','BR':'Bihar',
          'CH':'Chandigarh','CT':'Chhatisgarh','DL':'Delhi','DN':'Daman & Diu','GA':'Goa','GJ':'Gujarat','HP':'Himachal Pradesh',
          'HR':'Hariyana','JH_x':'Jharkhand','JK_x':'Jammu & Kashmir','KA_x':'Karnataka','LA_x':'Ladakh','MH_x':'Maharashtra',
@@ -28,181 +61,27 @@ to_select = ['KL','AN', 'AP', 'AR', 'AS', 'BR', 'CH', 'CT', 'DL', 'DN', 'GA', 'G
              'HP','HR','JH', 'JK', 'KA', 'LA', 'MH', 'ML', 'MN', 'MP', 'MZ', 'NL',
              'JH', 'JK', 'KA', 'LA', 'MH', 'ML', 'MN', 'MP', 'MZ', 'NL', 'OR', 'PB', 
              'PY', 'RJ', 'SK', 'TG', 'TN', 'TR', 'TT', 'UN', 'UP', 'UT','WB']
-
-
-def Confirmed(li):
-    """ Fetches and organises the data for confirmed cases in each state """
-    
-    dt=[]
-    des=[]
-    
-    for col in li:
-        if col=='KL':
-            for dat in df[col]['dates'].items():
-                dt.append(dat[0])
-                try:
-                    des.append(dat[1]['total']['confirmed'])
-                except:
-                    des.append('NaN')
-            df2 = pd.concat([pd.DataFrame(dt,columns=['Date']), pd.DataFrame(des,columns=['KL'])],axis=1)
         
-        dt2=[]
-        des2=[]
-        if col != 'KL':   
-            df3 = pd.DataFrame()
-            for dat in df[col]['dates'].items():
-                dt2.append(dat[0])
-                try:
-                    des2.append(dat[1]['total']['confirmed'])
-                except:
-                    des2.append('NaN')
-            df3 = pd.concat([pd.DataFrame(dt2,columns=['Date']), pd.DataFrame(des2,columns=[col])],axis=1)
-            
-            df2 = pd.merge_ordered(df2,df3,on='Date')
-            
-    return df2
-       
-        
-df2 = Confirmed(to_select)
+df2 = get_total_data(to_select,'confirmed')
 confirm2 = df2.set_index('Date') 
-confirm = df2.drop(to_drop, axis = 1)
-confirm = confirm.rename(columns=dicti)
-confirm = confirm.replace('NaN',0)
+confirm = df2.drop(to_drop, axis = 1).rename(columns=dicti).replace('NaN',0)
 total_c = confirm[['India']]
           
-def Deceased(li):
-    """ Fetches and organises the data for Deceased cases in each state """
-    
-    dt_de=[]
-    des_de=[]
-    
-    for col in li:
-        if col=='KL':
-            for dat in df[col]['dates'].items():
-                dt_de.append(dat[0])
-                try:
-                    des_de.append(dat[1]['total']['deceased'])
-                except:
-                    des_de.append('NaN')
-            df2 = pd.concat([pd.DataFrame(dt_de,columns=['Date']), pd.DataFrame(des_de,columns=['KL'])],axis=1)
-        dt2_de=[]
-        des2_de=[]
-        if col != 'KL':   
-            df3 = pd.DataFrame()
-            for dat in df[col]['dates'].items():
-                dt2_de.append(dat[0])
-                try:
-                    des2_de.append(dat[1]['total']['deceased'])
-                except:
-                    des2_de.append('NaN')
-            df3 = pd.concat([pd.DataFrame(dt2_de,columns=['Date']), pd.DataFrame(des2_de,columns=[col])],axis=1)
-            df3['Date'] = pd.to_datetime(df3['Date'])
-            df2['Date'] = pd.to_datetime(df2['Date'])
-            df2 = pd.merge_asof(df2,df3,on='Date')
-    
-    return df2
-
-deaths = Deceased(to_select)
+deaths = get_total_data(to_select,'deceased')
 deaths2 = deaths.set_index('Date')
-deaths = deaths.drop(to_drop,axis=1)
-deaths = deaths.rename(columns=dicti)
-deaths = deaths.replace('NaN',0)
+deaths = deaths.drop(to_drop,axis=1).rename(columns=dicti).replace('NaN',0)
 total_d = deaths[['India']]
 
-def Recovered(li):
-    """ Fetches and organises the data for Recovered cases in each state """
-    dt=[]
-    des=[]
-    
-    for col in li:
-        if col=='KL':
-            for dat in df[col]['dates'].items():
-                dt.append(dat[0])
-                try:
-                    des.append(dat[1]['total']['recovered'])
-                except:
-                    des.append('NaN')
-            df2 = pd.concat([pd.DataFrame(dt,columns=['Date']), pd.DataFrame(des,columns=['KL'])],axis=1)
-        
-        dt2=[]
-        des2=[]
-        if col != 'KL':   
-            df3 = pd.DataFrame()
-            for dat in df[col]['dates'].items():
-                dt2.append(dat[0])
-                try:
-                    des2.append(dat[1]['total']['recovered'])
-                except:
-                    des2.append('NaN')
-            df3 = pd.concat([pd.DataFrame(dt2,columns=['Date']), pd.DataFrame(des2,columns=[col])],axis=1)
-            
-            df2 = pd.merge(df2,df3,on=['Date'], how='outer')
-            
-    return df2
-       
-        
-recovery = Recovered(to_select)
+recovery = get_total_data(to_select,'recovered')
 recovery2 = recovery.set_index('Date')  
-recovery = recovery.drop(to_drop, axis = 1)
-recovery = recovery.rename(columns=dicti)
-recovery = recovery.replace('NaN',0)
+recovery = recovery.drop(to_drop, axis = 1).rename(columns=dicti).replace('NaN',0)
 total_r = recovery[['India']]
 
-def Tested(li):
-    """ Fetches and organises the data for Total Tests in each state """
-    dt=[]
-    des=[]
-    
-    for col in li:
-        if col=='KL':
-            for dat in df[col]['dates'].items():
-                dt.append(dat[0])
-                try:
-                    des.append(dat[1]['total']['tested'])
-                except:
-                    des.append('NaN')
-            df2 = pd.concat([pd.DataFrame(dt,columns=['Date']), pd.DataFrame(des,columns=['KL'])],axis=1)
-        
-        dt2=[]
-        des2=[]
-        if col != 'KL':   
-            df3 = pd.DataFrame()
-            for dat in df[col]['dates'].items():
-                dt2.append(dat[0])
-                try:
-                    des2.append(dat[1]['total']['tested'])
-                except:
-                    des2.append('NaN')
-            df3 = pd.concat([pd.DataFrame(dt2,columns=['Date']), pd.DataFrame(des2,columns=[col])],axis=1)
-            
-            df2 = pd.merge(df2,df3,on=['Date'], how='outer')
-            
-    return df2
-       
-        
-test = Tested(to_select)
-
+test = get_total_data(to_select,'tested')
 test['Date'] = pd.to_datetime(test['Date'])
 test2 = test.set_index('Date')
-test = test.drop(to_drop, axis = 1)
-test = test.rename(columns=dicti)
-test = test.replace('NaN',0)
+test = test.drop(to_drop, axis = 1).rename(columns=dicti).replace('NaN',0)
 total_t = test[['India']]
-
-################# STREAMLIT #######################
-
-st.title('Covid-19 Dashboard India')
-
-st.sidebar.header('EXPLORE DASHBOARD')
-st.sidebar.markdown(""" <ul>
-                        <li>India : Total cases in India Timeline</li>
-                        <li>Statewise : Analysis according to states </li>
-                        <li>Analysis between a time period</li>
-                        </ul>
-                    """, unsafe_allow_html=True)
-
-st.sidebar.subheader('Select an option')
-rad = st.sidebar.radio('',['India Total','Statewise','For Particular Time'])
 
 total_c = total_c.rename(columns={'India': 'confirm'}) 
 total_d = total_d.rename(columns={'India': 'deaths'})
@@ -213,8 +92,29 @@ total = total.replace('NaN',0)
 total['active'] = (total['confirm'] - total['recovery']) - total['deaths']
 
 lis = list(dicti.values())
+con = total.iloc[-1,1]
+deat = total.iloc[-1,2]
+reco = total.iloc[-1,3]
+tes = total.iloc[-1,4]
+act = total.iloc[-1,5]
+table = pd.concat([pd.Series(con),pd.Series(deat),pd.Series(reco),pd.Series(act),pd.Series(tes)],axis=1)
+table = table.rename(columns={0:'Confirmed',1:'Deaths',2:'Recovery',3:'Active',4:'Tests'}).set_index('Confirmed')
 
-if rad == 'India Total':
+################# STREAMLIT #######################
+
+st.title('Covid-19 Dashboard India')
+st.sidebar.header('EXPLORE DASHBOARD')
+st.sidebar.markdown(""" <ul>
+                        <li>India : Total cases in India Timeline</li>
+                        <li>Statewise : Analysis according to states </li>
+                        <li>Analysis between a time period</li>
+                        </ul>
+                    """, unsafe_allow_html=True)
+
+st.sidebar.subheader('Select an option')
+radio = st.sidebar.radio('',['India Total','Statewise','For Particular Time'])
+
+if radio == 'India Total':
     
     st.markdown("""     <div style='background-color:white;padding:10px'>
                     <h4 style='color:black'><i> About Dashboard </style></h4>
@@ -224,7 +124,7 @@ if rad == 'India Total':
                         <li>Hover over plots for more Detailed Information</li>
                         </ol>
                     """, unsafe_allow_html=True)
-    
+    st.table(table)     
     def plot_bar_ind(data,y1,title):
             """Plots the Stacked Bar chart for overall cases in Country"""
             
@@ -255,14 +155,13 @@ if rad == 'India Total':
             #fig.layout.paper_bgcolor='#fff'
             st.plotly_chart(fig)
             
-            st.subheader('Covid-19 Data India')
-            st.dataframe(data1)
+            st.subheader('Covid-19 Data India (last 10 Days)')
+            st.table(data1.set_index('Date')[-10:])
     plot_line_ind(total[40:],['active','deaths','recovery'],'Line Chart Showing Covid-19 Timeline') 
     
-if rad == 'Statewise':
+if radio == 'Statewise':
     st.subheader('Select Data')
     fetch = st.selectbox(' ',['Confirmed Cases','Deaths','Recoveries','Tests'])
-
 
     def figure_plot_state(data,states,title,color):
         """Plots data for Active, Deceased, Recovered and Total Tests for the States selected"""
@@ -281,7 +180,7 @@ if rad == 'Statewise':
         st.plotly_chart(fig)
         st.subheader(f'{title} Data'.format(title))
         data = data.set_index('Date')
-        st.dataframe(data[states])
+        st.table(data[states][-10:])
         
     try:
         states = st.multiselect('  ',lis,default=['Rajasthan','Delhi','Maharashtra'])
@@ -298,9 +197,8 @@ if rad == 'Statewise':
         figure_plot_state(test,states,'Total Tests','blue')
         
 
-if rad == 'For Particular Time':
-    st.markdown("""     
-                        <ul>
+if radio == 'For Particular Time':
+    st.markdown("""     <ul>
                         <li>End Date should be greater than Start Date</li>
                         <li>Select Checkbox to Visualize and Analyze</li>
                         </ul>
@@ -324,7 +222,7 @@ if rad == 'For Particular Time':
         #fig.layout.paper_bgcolor='#fff'
         st.plotly_chart(fig)
         st.subheader('Data for selected Dates')
-        st.dataframe(data2[y1])
+        st.dataframe(data2.set_index('Date')[y1])
     
     confirm3 = confirm.copy()
     if st.sidebar.checkbox('Visualize'):
@@ -337,10 +235,4 @@ if rad == 'For Particular Time':
             st.header('End Date is Smaller than Start Date.....')
                 
 st.sidebar.success('author : @Koshal')     
-            
-    
-
-        
-    
-    
     
